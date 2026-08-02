938a9b333523e4e74ac560ff7d76da3750e3cc44
enidyz <enidyz44@gmail.com>
2026-08-03 00:26:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -53,4 +53,4 @@ toTop?.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: reducedMotion ? 'auto' : 'smooth' });
 });
 
-window.addEventListener('scroll', closeDrawer, { passive: true });
+window.addEventListener('scroll', closeDrawer, { passive: true }); 
